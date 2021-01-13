5a490b7b42414e217a2ae80fd8b164c092a429d5
mytun <lxyg06@163.com>
2021-01-13 09:39:17 +0800

    配置完成

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import { registerMicroApps, start } from 'qiankun';
 Vue.config.productionTip = false
 
+
+registerMicroApps([
+    {
+        name: 'vue', // app name registered
+        entry: '//localhost:7100',
+        container: '#vue',
+        activeRule: '/vue',
+    }
+]);
+start();
 new Vue({
   router,
   render: h => h(App)
